a6e96662fe6fef96cfffca04898e3358a93f29c8
wa1tnr <wa1tnr@users.noreply.github.com>
2017-06-06 20:45:29 +0000

    new file:   src/kernel/dot_quote.cpp 	new file:   src/kernel/dot_quote.h 	modified:   flashDict.cpp 	modified:   flashDict.h

---
+++ src/kernel/dot_quote.cpp
@@ -0,0 +1,14 @@
+// Tue Jun  6 20:42:31 UTC 2017
+// 4733-a9a
+
+#include <Arduino.h>
+#include "../../yaffa.h"
+#include "dot_quote.h"
+
+extern const char dot_quote_str[]; // = ".\x22";
+void _dot_quote(void) {
+  // char length;
+  char length 0;
+  length = 1; // nop
+}
+
---
+++ src/kernel/dot_quote.h
@@ -0,0 +1,9 @@
+// Tue Jun  6 20:42:31 UTC 2017
+// 4733-a9a
+
+#ifndef DOT_QUOTE
+#define DOT_QUOTE
+
+extern cell_t* pHere;
+
+#endif
