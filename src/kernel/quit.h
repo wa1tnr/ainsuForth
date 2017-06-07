45a10ab429f4dcba4122a3b1af8a1a6c145742d9
wa1tnr <wa1tnr@users.noreply.github.com>
2017-06-07 01:28:16 +0000

    The quit word's headers.

---
+++ src/kernel/quit.h
@@ -0,0 +1,15 @@
+// Wed Jun  7 01:08:31 UTC 2017
+// 4735-a0a
+
+// Tue Jun  6 22:16:24 UTC 2017
+// 4733-a9b
+
+#ifndef KERNEL_QUIT
+#define KERNEL_QUIT
+
+extern void _quit(void);
+extern void rStack_clear(void);
+extern char* cpToIn;
+
+
+#endif
