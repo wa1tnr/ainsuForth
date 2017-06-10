e12b209dca9fd3b55ca95d74de0d4d3b403b7b33
wa1tnr <wa1tnr@users.noreply.github.com>
2017-06-10 02:59:40 +0000

    new file:   literal.cpp 	new file:   literal.h

---
+++ src/kernel/literal.h
@@ -0,0 +1,21 @@
+// Fri Jun  9 02:32:35 UTC 2017
+// 4735-a0f
+
+#ifndef KERN_LITERAL
+#define KERN_LITERAL
+
+// prototypes - this word
+extern const char literal_str[]; // = "literal";
+extern void _literal(void);
+
+// prototypes - variables
+extern cell_t* pHere;               // HERE, points to the next free position in
+                                    // Forth Space
+extern uint8_t state; // Holds the text interpreters compile/interpreter state
+extern cell_t* ip;   // Instruction Pointer
+
+// prototypes - external functions
+extern void dStack_push(cell_t value);
+extern cell_t dStack_pop(void);
+
+#endif
