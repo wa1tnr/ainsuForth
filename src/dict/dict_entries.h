e793f148fef4aed6eaa4560f3a0fc731239a1935
wa1tnr <wa1tnr@users.noreply.github.com>
2017-06-20 21:16:28 +0000

    new file:   src/dict/dict_entries.cpp 	new file:   src/dict/dict_entries.h

---
+++ src/dict/dict_entries.h
@@ -0,0 +1,21 @@
+// Tue Jun 20 18:57:54 UTC 2017
+// 4735-a0p-01-
+
+extern const char abs_str[]; // = "abs";
+extern void _abs(void);
+
+extern const char zero_equal_str[]; // = "0=";
+extern void _zero_equal(void);
+
+extern const char and_str[]; // = "and";
+extern void _and(void);
+
+extern const char count_str[]; // = "count";
+extern void _count(void);
+
+extern const char word_str[]; // = "word";
+extern void _word(void);
+
+extern const char xor_str[]; // = "xor";
+extern void _xor(void);
+
