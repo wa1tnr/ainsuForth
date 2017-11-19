cb9f60758255be1360544318b1011e05c28affc0
wa1tnr <wa1tnr@users.noreply.github.com>
2017-11-19 04:54:58 +0000

    timestamped:   ainsuForth.ino 	+testLED command line with emits:   doc/HARDWARE.local.txt

--- ainsuForth.ino
+++ ainsuForth.ino
@@ -1,15 +1,16 @@
-// Sun Nov 19 04:45:07 UTC 2017
-// 4735-b0c-07h-   the -07x- is new Nov 19, 2017.
+// Sun Nov 19 04:53:29 UTC 2017
+// 4735-b0c-07j-   the -07x- is new Nov 19, 2017.
 
 // previous timestamps:
+// Sun Nov 19 04:45:07 UTC 2017
 // Sun Nov 19 03:57:49 UTC 2017
 // Sun Nov 19 02:54:46 UTC 2017
 // Sat Jul 29 21:25:24 UTC 2017
 
 // for Gemma M0 19 November 2017 - this can and does change.  Presently, correct, though.
 
-// KAIHU   TENFEET
-// CPHUU   KELEFICOO   MICHANDULO
+// LIGGX   KAIHU   TENFEET
+// CPHUU   KELEFICOO
 
 // KEPOK  WHELBUP
 
