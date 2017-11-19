268d2dd134f71c6dbbf2541528b955042d356c90
wa1tnr <wa1tnr@users.noreply.github.com>
2017-11-19 06:05:09 +0000

    timestamped:   ainsuForth.ino

--- ainsuForth.ino
+++ ainsuForth.ino
@@ -1,7 +1,8 @@
-// Sun Nov 19 05:38:40 UTC 2017
-// 4735-b0c-07p-   the -07x- is new Nov 19, 2017.
+// Sun Nov 19 06:03:05 UTC 2017
+// 4735-b0c-07r-   the -07x- is new Nov 19, 2017.
 
 // previous timestamps:
+// Sun Nov 19 05:38:40 UTC 2017
 // Sun Nov 19 05:17:34 UTC 2017
 // Sun Nov 19 04:45:07 UTC 2017
 // Sun Nov 19 03:57:49 UTC 2017
